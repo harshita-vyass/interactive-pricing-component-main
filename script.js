181aa64375e10598810c1622761ffaa0956a9546
Harshita Vyas <132072016+harshita-vyass@users.noreply.github.com>
2025-05-20 04:16:20 +0530

    update design

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
+const slider = document.getElementById("slider");
+const billingToggle = document.getElementById("billingToggle");
+const price = document.getElementById("price");
+const pageviews = document.getElementById("pageviews");
 
-function priceChangeHandler(){
-    var range= document.getElementById("priceRange").value;
-    document.getElementById('priceRanges').innerHTML = range;
+const pricingData = [
+  { views: "10K", monthly: 8 },
+  { views: "50K", monthly: 12 },
+  { views: "100K", monthly: 16 },
+  { views: "500K", monthly: 24 },
+  { views: "1M", monthly: 36 },
+];
+function updateSliderBackground() {
+  const min = slider.min;
+  const max = slider.max;
+  const val = slider.value;
 
+  const percentage = ((val - min) / (max - min)) * 100;
+
+  slider.style.background = `linear-gradient(to right, var(--Strong_Cyan) ${percentage}%, var(--Grayish_Blue) ${percentage}%)`;
 }
 
-//arrow function
-const sliderprogress= ()=> {
-    var sliderValue = document.getElementById("priceRange").value;
-    document.getElementById("priceRange").style.background = `linear-gradient(90deg,hsl(174, 77%, 80%)  ${sliderValue}%, hsl(225, 20%, 60%) ${sliderValue}%)`;
-  }
+function updatePricing() {
+  const index = slider.value;
+  const isYearly = billingToggle.checked;
+  const monthlyPrice = pricingData[index].monthly;
+  const finalPrice = isYearly ? (monthlyPrice * 12 * 0.75 / 12).toFixed(2) : monthlyPrice.toFixed(2);
   
+  pageviews.textContent = `${pricingData[index].views} Pageviews`;
+  price.textContent = `$${finalPrice}`;
+  updateSliderBackground()
+}
+
+slider.addEventListener("input", updatePricing);
+billingToggle.addEventListener("change", updatePricing);
+
+updatePricing(); 
